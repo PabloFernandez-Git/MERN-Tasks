c997a3cec166407606d7c9b03ff72264b7e3d50d
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-08-13 16:27:57 -0300

    Creando los componentes y routing

---
+++ client/src/components/auth/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+    return (  
+        <h1>Desde login</h1>
+    );
+}
+ 
+export default Login;
---
+++ client/src/components/auth/NuevaCuenta.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NuevaCuenta = () => {
+    return (  
+        <h1>Desde nueva cuenta</h1>
+    );
+}
+ 
+export default NuevaCuenta;
