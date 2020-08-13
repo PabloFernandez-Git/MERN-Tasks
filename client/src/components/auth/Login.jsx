ca8a3d2488b5a2d2a7f434ef925be866b638f1ca
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-08-13 16:50:10 -0300

    Creando el Formulario de Login

--- client/src/components/auth/Login.jsx
+++ client/src/components/auth/Login.jsx
@@ -1,8 +1,45 @@
 import React from 'react';
 
 const Login = () => {
+
+    const onChange = () => {
+
+    }
+
     return (  
-        <h1>Desde login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sesion</h1>
+
+                <form>
+                    <div className="campo-form">
+                       <label htmlFor="email">Email</label>
+                       <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="Ingrese su correo"
+                            onChange={onChange}
+                       /> 
+                    </div>
+
+                    <div className="campo-form">
+                       <label htmlFor="password">Password</label>
+                       <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Ingrese su contraseña"
+                            onChange={onChange}
+                       /> 
+                    </div>
+
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Iniciar sesion" />
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
  
