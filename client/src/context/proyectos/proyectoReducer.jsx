2b5f63d87afb25a2f285439c042c19f139dd7ef1
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-08-16 17:45:36 -0300

    Agregando la función para mostrar el Formulario

--- client/src/context/proyectos/proyectoReducer.jsx
+++ client/src/context/proyectos/proyectoReducer.jsx
@@ -1,6 +1,14 @@
+import { FORMULARIO_PROYECTO } from '../../types/index';
+
 
 export default (state, action) => {
     switch(action.type) {
+        case FORMULARIO_PROYECTO:
+            return {
+                ...state,
+                formulario: true
+            }
+
         default:
             return state;
     }
--- client/src/context/proyectos/proyectoState.jsx
+++ client/src/context/proyectos/proyectoState.jsx
@@ -2,6 +2,7 @@ import React, { useReducer } from 'react';
 
 import proyectoContext from './proyectoContext';
 import proyectoReducer from './proyectoReducer';
+import { FORMULARIO_PROYECTO } from '../../types/index';
 
 const ProyectoState = props => {
     const initialState = {
@@ -12,11 +13,17 @@ const ProyectoState = props => {
     const [state, dispatch] = useReducer(proyectoReducer, initialState)
 
     // Serie de funciones para el CRUD
+    const mostrarFormulario = () => {
+        dispatch({
+            type: FORMULARIO_PROYECTO
+        })
+    }
 
     return (
         <proyectoContext.Provider
             value={{
-                formulario: state.formulario
+                formulario: state.formulario,
+                mostrarFormulario
             }}
         >
             {props.children}
