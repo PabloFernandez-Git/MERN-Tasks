9cfebee399451c08ef579e21a41df96e69f2da81
Pablo Fernandez <pablo.ezequiel.fernandez@gmail.com>
2020-08-19 15:27:35 -0300

    Mostrando el Nombre del Proyecto

--- client/src/components/tareas/ListadoTareas.jsx
+++ client/src/components/tareas/ListadoTareas.jsx
@@ -1,8 +1,19 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import Tarea from './Tarea';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 
 const ListadoTareas = () => {
+
+    // Extraer proyectos de state inicial
+    const proyectosContext = useContext(proyectoContext);
+    const { proyecto } = proyectosContext;
+
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return <h2>Selecciona un proyecto</h2>;
+
+    // Array destructuring para extraer el proyecto actual
+    const [proyectoActual] = proyecto;
 
     const tareasProyecto = [
         {nombre: 'Elegir plataforma', estado: true},
@@ -13,7 +24,7 @@ const ListadoTareas = () => {
 
     return (
         <Fragment>
-            <h2>Proyecto: Tienda Virtual</h2>
+            <h2>Proyecto: {proyectoActual.nombre} </h2>
 
             <ul className="listado-tareas">
                 {tareasProyecto.length ===  0
